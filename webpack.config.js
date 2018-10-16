1465c291b153010377db925dbb867e055900cbdb
Robert Gao <gaorober@usc.edu>
2018-10-16 14:17:15 -0700

    adds babel polyfill for async await functionality

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const DIST_DIR = path.join(__dirname, '/public');
 
 module.exports = {
   devtool: 'eval-source-map',
-  entry: `${SRC_DIR}/index.jsx`,
+  entry: ['babel-polyfill', `${SRC_DIR}/index.jsx`],
   output: {
     filename: 'bundle.js',
     path: DIST_DIR,
