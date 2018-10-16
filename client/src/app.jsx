45dc608fc78cde2b39c52a841a9e97444cd5afbb
Robert Gao <gaorober@usc.edu>
2018-10-15 22:06:37 -0700

    calls all apis successfully

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import $ from 'jquery';
 
 
 class App extends Component {
@@ -8,50 +9,96 @@ class App extends Component {
     this.state = {
       lyft: [],
       uber: [],
+      login: false,
     };
 
+    this.comparePrices = this.comparePrices.bind(this);
     this.checkLyft = this.checkLyft.bind(this);
+    this.checkUber = this.checkUber.bind(this);
+    this.login = this.login.bind(this);
   }
 
   componentDidMount() {
     // this.checkLyft();
   }
 
-  checkLyft() {
-    axios.get('/checkLyft')
+  comparePrices(start, end) {
+    axios.get('/api/geocode', {
+      params: {
+        start: $('#start').val()'944 Market St San Francisco CA',
+        end: '44 Tehama St San Francisco CA',
+      },
+    })
+      .then((data) => {
+        const locations = {
+          start: data.data.start,
+          end: data.data.end,
+        }
+        console.log(locations);
+        this.checkLyft(locations.start, locations.end);
+        this.checkUber(locations.start, locations.end);
+      })
+      // .catch(err => console.log(err));
+  }
+
+  checkLyft(start, end) {
+    console.log(start, end);
+    axios.get('/checkLyft', {
+      params: { start, end },
+    })
       .then(data => this.setState({
         lyft: data.data,
       }))
       .catch(err => console.log(err));
   }
 
-  checkUber() {
-    axios.get('/checkUber')
+  checkUber(start, end) {
+    console.log(start, end);
+    axios.get('/checkUber', {
+      params: { start, end },
+    })
       .then(data => this.setState({
         uber: data.data,
       }))
       .catch(err => console.log(err));
   }
 
+  login() {
+    axios.get('/api/login')
+      .then(() => this.setState({
+        login: true,
+      }))
+      .catch(err => console.log(err));
+  }
+
   render() {
-    const { lyft, uber } = this.state;
+    const { login, lyft, uber } = this.state;
     console.log(lyft);
     return (
       <div>
-        <button type="button" onClick={this.checkLyft}>Check Lyft</button>
-        <button type="button" onClick={this.checkUber}>Check Uber</button>
+        <button type="button" onClick={this.login}>Log In</button>
+        <form>
+          <input type="text" placeholder="Start Address" />
+          <input type="text" placeholder="End Address" />
+          <button type="button" onClick={this.comparePrices}>Compare Prices!</button>
+          <button type="button" onClick={this.test}>Test</button>
+        </form>
         <div>Hello React!</div>
-        <div>
-          {lyft.map(ride => (
-            <div>
-              {`${ride.display_name} | $${(ride.estimated_cost_cents_min + ride.estimated_cost_cents_max) / 200} | ${ride.estimated_duration_seconds}s`}
-            </div>
-          ))}
-        </div>
-        <div>
-          {uber.map(ride => (
-            <div>{ride}</div>
-          ))}
+        <div id="compare" className="hello">
+          <div id="lyft">
+            {lyft.map(ride => (
+              <div>
+                {`${ride.display_name} | $${(ride.estimated_cost_cents_min / 200)} - ${(ride.estimated_cost_cents_max / 200)} | ${ride.estimated_duration_seconds}s`}
+              </div>
+            ))}
+          </div>
+          <div id="uber">
+            {uber.map(ride => (
+              <div>
+                {`${ride.localized_display_name} | ${ride.estimate} | ${ride.distance} | ${ride.duration}s`}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     );
--- server/index.js
+++ server/index.js
@@ -1,11 +1,57 @@
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 const path = require('path');
 const lyft = require('node-lyft');
+const Uber = require('node-uber');
 const keys = require('../apiKeys');
 
 const app = express();
 app.use(cors());
+
+// app.use(express.static(__dirname + '/../public'));
+
+var uber = new Uber({
+  client_id: keys.uber.client_id,
+  client_secret: keys.uber.client_secret,
+  server_token: keys.uber.server_token,
+  redirect_uri: 'http://127.0.0.1:8008/api/callback',
+  name: 'Loober',
+  language: 'en_US', // optional, defaults to en_US
+  sandbox: true
+});
+
+app.get('/api/login', (req, res) => {
+  var url = uber.getAuthorizeUrl(['history','profile', 'request', 'places']);
+  console.log(url);
+  res.redirect(url);
+})
+
+app.get('/api/geocode', (req, res) => {
+  const params = {
+    address: req.query.start,
+    key: keys.google
+  }
+  axios.get('https://maps.googleapis.com/maps/api/geocode/json', { params })
+    .then((data) => {
+      const location = data.data.results[0].geometry.location;
+      const params = {
+        address: req.query.end,
+        key: keys.google
+      }
+      axios.get('https://maps.googleapis.com/maps/api/geocode/json', { params })
+        .then((data) => {
+          const locations = {
+            start: location,
+            end: data.data.results[0].geometry.location
+          }
+          console.log(locations);
+          res.send(locations);
+        })
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err));
+})
 
 app.get('/checkLyft', (req, res) => {
   let defaultClient = lyft.ApiClient.instance;
@@ -14,13 +60,18 @@ app.get('/checkLyft', (req, res) => {
   clientAuth.accessToken = keys.lyft;
 
   let apiInstance = new lyft.PublicApi();
-
+  const start = JSON.parse(req.query.start);
+  const end = JSON.parse(req.query.end);
+  console.log(start, end);
+  
   let opts = { 
-    'endLat': 37.7972,
-    'endLng': -122.4533
+    // 'endLat': 37.7972,
+    // 'endLng': -122.4533
+    endLat: end.lat,
+    endLng: end.lng,
   };
 
-  apiInstance.getCost(37.7763, -122.3918, opts).then((data) => {
+  apiInstance.getCost(start.lat, start.lng, opts).then((data) => {
     res.send(data.cost_estimates);
   }, (error) => {
     console.error(error);
@@ -28,7 +79,34 @@ app.get('/checkLyft', (req, res) => {
   });
 })
 
+app.get('/api/callback', function(req, res) {
+  uber.authorizationAsync({authorization_code: req.query.code})
+  .spread(function(access_token, refresh_token, authorizedScopes, tokenExpiration) {
+    // store the user id and associated access_token, refresh_token, scopes and token expiration date
+    console.log('New access_token retrieved: ' + access_token);
+    console.log('... token allows access to scopes: ' + authorizedScopes);
+    console.log('... token is valid until: ' + tokenExpiration);
+    console.log('... after token expiration, re-authorize using refresh_token: ' + refresh_token);
+
+    // redirect the user back to your actual app
+    res.redirect('/');
+  })
+  .error(function(err) {
+    console.error(err);
+  });
+});
+
 app.get('/checkUber', (req, res) => {
+
+  const start = JSON.parse(req.query.start);
+  const end = JSON.parse(req.query.end);
+  // uber.estimates.getPriceForRouteAsync(start_latitude, start_longitude, end_latitude, end_longitude [, seats]);
+    uber.estimates.getPriceForRouteAsync(start.lat, start.lng, end.lat, end.lng)
+    .then(function(data) { 
+      console.log(data);
+      res.send(data.prices);
+    })
+    .error(function(err) { console.error(err); });
 
 })
 
@@ -36,7 +114,11 @@ app.get('/bundle.js', (req, res) => {
   res.sendFile(path.resolve('public/bundle.js'));
 });
 
-app.get('*', (req, res) => {
+app.get('/styles.css', (req, res) => {
+  res.sendFile(path.resolve('public/styles.css'))
+})
+
+app.get('/', (req, res) => {
   res.sendFile(path.resolve('public/index.html'));
 });
 
